078e7314355495e490257644746829b9b1545ecb
harry <harshitsharma8800@gmail.com>
2022-06-06 09:33:26 +0530

    Day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class item {
+  final String? id;
+  final String? name;
+  final String? desc;
+  final num? price;
+  final String? color;
+  final String? image;
+
+  item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  item(
+      id: "1",
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 10000,
+      color: "#33505a",
+      image:
+          "")
+];
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -13,13 +13,14 @@ class MyDrawer extends StatelessWidget {
             DrawerHeader(
                 padding: EdgeInsets.zero,
                 child: UserAccountsDrawerHeader(
-                  margin: EdgeInsets.zero,
-                  accountName: Text("Harshit SHarma"),
-                  accountEmail: Text("harshitsharma8800@gmail.com"),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(""),
-                  ),
-                )),
+                    margin: EdgeInsets.zero,
+                    accountName: Text("Harshit SHarma"),
+                    accountEmail: Text("harshitsharma8800@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                        backgroundImage:
+                            // NetworkImage("assets/images/myimg.png")
+                            NetworkImage(
+                                "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")))),
             ListTile(
               leading: Icon(
                 CupertinoIcons.home,
